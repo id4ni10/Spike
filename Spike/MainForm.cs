98e52afda7b12ae57e4a0eca8079b544a188ce07
Danilo Nascimento Cunha <id4ni10@gmail.com>
2017-11-10 01:32:12 -0300

    generate output

--- Spike/MainForm.cs
+++ Spike/MainForm.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Spike
 {
     public partial class MainForm : Form
     {
+        private List<Entry> list = new List<Entry>();
+
         public MainForm()
         {
             InitializeComponent();
@@ -24,18 +23,18 @@ namespace Spike
 
             var logs = EventLog.GetEventLogs().Where(log => log.Log.Equals("System"));
 
-            var result = (from EventLogEntry entry in logs.First().Entries
-                          where dateSearch.Value.Month == entry.TimeGenerated.Date.Month & dateSearch.Value.Year == entry.TimeGenerated.Date.Year
-                          group entry by entry.TimeGenerated.Date into dias
-                          orderby dias.Key descending
-                          select new Entry
-                          {
-                              Dia = dias.First().TimeGenerated.Date.ToString("dd/MM/yyyy"),
-                              Entrada = dias.First().TimeGenerated.ToString("HH:mm:ss"),
-                              Saida = dias.Last().TimeGenerated.ToString("HH:mm:ss")
-                          }).ToList();
+            list = (from EventLogEntry entry in logs.First().Entries
+                    where dateSearch.Value.Month == entry.TimeGenerated.Date.Month & dateSearch.Value.Year == entry.TimeGenerated.Date.Year
+                    group entry by entry.TimeGenerated.Date into dias
+                    orderby dias.Key descending
+                    select new Entry
+                    {
+                        Dia = dias.First().TimeGenerated.Date.ToString("dd/MM/yyyy"),
+                        Entrada = dias.First().TimeGenerated.ToString("HH:mm:ss"),
+                        Saida = dias.Last().TimeGenerated.ToString("HH:mm:ss")
+                    }).ToList();
 
-            gridView.DataSource = result;
+            gridView.DataSource = list;
 
             gridView.Refresh();
 
@@ -44,7 +43,19 @@ namespace Spike
 
         private void buttonExportar_Click(object sender, EventArgs e)
         {
+            this.Cursor = Cursors.WaitCursor;
 
+            var content = (from value in list select string.Format("{0},{1},{2}", value.Dia, value.Entrada, value.Saida)).ToList();
+
+            content.Insert(0, "Dia,Entrada,Saida");
+
+            var file = Path.Combine(Environment.CurrentDirectory, string.Format("{0}{1}", DateTime.Now.Ticks, ".csv"));
+
+            File.WriteAllLines(file, content);
+
+            Process.Start(file);
+
+            this.Cursor = Cursors.Default
         }
     }
 
